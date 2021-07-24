e3472700fabcd99921c513325d509d89a1cb7447
VitaliiKor <me8@yandex.ru>
2021-07-24 22:04:13 +0700

    Выполненное Д.з. по теме Функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -14,9 +14,9 @@ function getArrayParams(arr) {
 	  }
   }
 
-    for (j=0; j<arr.length; j++) {
+  for (j=0; j<arr.length; j++) {
 		sum += arr[j];
-	}
+  }
 	avg = (sum/arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
@@ -24,23 +24,36 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+    for (k=0; k<arr.length; k++) {
+		sum += arr[k];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (l=0; l<arrOfArr.length; l++){
+	  if (func(arrOfArr[l])>max){
+		  max=func(arrOfArr[l]);
+	  }
+  }
   return max
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = arr[0];
+  let min = arr[0];
+  let diff;
+  for (m=0; m<arr.length; m++){
+	  if (arr[m]>max){
+		  max=arr[m];
+	  }
+	  	  if (arr[m]<min) {
+		  min=arr[m];
+	  }
+  }
+  diff = max - min;
+  return diff;
 }

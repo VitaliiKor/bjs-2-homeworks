c6b15b2bcbeadb16e7b99ea60ce1f2b7c518010c
VitaliiKor <me8@yandex.ru>
2021-07-26 10:50:25 +0700

    Корректировка Д.з. после замечаний преподавателя

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,8 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
-  min = 100;
-  max = -100;
+  min = arr[0];
+  max = arr[0];
   sum = 0;
   
   for (j=0; j<arr.length; j++){
@@ -12,12 +12,9 @@ function getArrayParams(arr) {
 	  if (arr[j]>max){
 		  max=arr[j];
 	  }
+	  sum += arr[j];
   }
-
-  for (j=0; j<arr.length; j++) {
-		sum += arr[j];
-  }
-	avg = (sum/arr.length).toFixed(2);
+  avg = (sum/arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }

0ed3bf765c256e5886c04df690983c764e3e5b8f
VitaliiKor <me8@yandex.ru>
2021-07-28 21:52:50 +0700

    Д.з. 3, Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.every((n, i) => n === arr2[i]) && (arr1.length === arr2.length);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter((number) => number % 3 === 0).filter((number) => number > 0).map((number) => number * 10);
   return resultArr; // array
 }

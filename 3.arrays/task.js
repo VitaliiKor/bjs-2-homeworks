6019a27687532002b53b3d7395a11a4ebc6dd599
VitaliiKor <me8@yandex.ru>
2021-07-30 11:30:30 +0700

    Небольшая корректировка кода после замечаний преподавателя

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,15 @@
+  //Закомментированный код - то же самое, только чуть длиннее.
 function compareArrays(arr1, arr2) {
-  let result;
-  result = arr1.every((n, i) => n === arr2[i]) && (arr1.length === arr2.length);
-  return result; // boolean
+  // let result;
+  // result = arr1.every((n, i) => n === arr2[i]) && (arr1.length === arr2.length);
+  // return result; // boolean
+  return arr1.every((n, i) => n === arr2[i]) && (arr1.length === arr2.length);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-  resultArr = arr.filter((number) => number % 3 === 0).filter((number) => number > 0).map((number) => number * 10);
-  return resultArr; // array
+  // let resultArr;
+  // resultArr = arr.filter((number) => number % 3 === 0).filter((number) => number > 0).map((number) => number * 10);
+  // return resultArr; // array
+	return arr.filter((number) => number % 3 === 0).filter((number) => number > 0).map((number) => number * 10);
 }
+

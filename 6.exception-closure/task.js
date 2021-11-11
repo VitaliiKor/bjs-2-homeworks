df9688e872c7df038c407218e21d049acc7dc018
VitaliiKor <me8@yandex.ru>
2021-11-11 23:12:04 +0300

    20211111. Задание с ошибками на проверку

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,29 +1,41 @@
-//let val = 1;
-function counterGenerator(val) {
-	val = 3;
-	function showCounter() {
-		console.log(val);
-	}
-	function increaseCounter() {
-		val += 1;
-	}
-	function decCounter() {
-		val -= 5;
-	}
-	return [showCounter, increaseCounter, decCounter];
+let parseCount = (val) => {
+	if (Number.isNaN(Number.parseInt(5*val, 10))) throw "Невалидное значение";
+	return (Number.parseInt(val));
 }
-let [show, increase, dec] = counterGenerator();
 
-show();
-increase();
-show();
-dec();
-show();
-increase();
-show();
-dec();
-show();
-increase();
-increase();
-show();
+function validateCount() {
+	try {parseCount(val)
+	return parseCount(val);
+	} catch(e){
+	return console.log('Ошибка ' + e); // Вернуть ошибку
+	}
+}
 
+class Triangle {
+	constructor(a, b, c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (this.a + this.b < this.c || this.a + this.c < this.b || this.c + this.b < this.a){
+				return (console.log("Треугольник с такими сторонами не существует"));
+		}
+	
+	}
+		
+	getPerimeter(){
+		if (this.a + this.b > this.c & this.a + this.c > this.b & this.c + this.b > this.a){
+			return (console.log('Периметр треугольника = ' + (this.a+this.b+this.c) + ' единиц'));
+		} else {
+			return (console.log ("Треугольника с такими сторонами не существует"))
+		}
+	}
+	
+	getArea(){
+		let p = 0.5*(this.a+this.b+this.c);
+		if (this.a + this.b > this.c & this.a + this.c > this.b & this.c + this.b > this.a){
+		return(console.log('Площадь треугольника = ' + (Math.sqrt (p*(p-this.a)*(p-this.b)*(p-this.c))).toFixed(3) + ' квадратных единиц'));
+		} else {
+			return (console.log ("Треугольничка с такими сторонами не существует"))
+		}
+	}
+}
